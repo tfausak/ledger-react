510760dd048a0bb6fa95fa6b7a7cf94c488a1e93
Taylor Fausak <taylor@fausak.me>
2014-12-07 09:54:36 -0600

    Create a basic HTML page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var server = express();
+
+server.use(express.static('static'));
 
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
-express().listen(port, host, function () {
+server.listen(port, host, function () {
   console.info('http://' + host + ':' + port);
 });
