d3ca4b86c48c570e0e1b9da58cb0adb71c7fce00
Taylor Fausak <taylor@fausak.me>
2014-12-25 16:46:39 -0600

    Add the ability to collapse searching and filtering

--- src/scripts/components/search_and_filter.es6.jsx
+++ src/scripts/components/search_and_filter.es6.jsx
@@ -10,10 +10,45 @@ export default React.createClass({
 
   render: function () {
     return (
-      <div>
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          Search and filter
+
+          <div className="pull-right">
+            {this.renderPanelButton()}
+          </div>
+        </div>
+
+        {this.renderPanelBody()}
+      </div>
+    );
+  },
+  renderPanelButton: function () {
+    var icon = this.state.isOpen ? 'minus' : 'plus';
+    var className = ['glyphicon', 'glyphicon-' + icon].join(' ');
+
+    return (
+      <button className="btn btn-default btn-xs">
+        <span className={className} onClick={this.toggleOpen} />
+      </button>
+    );
+  },
+  renderPanelBody: function () {
+    if (!this.state.isOpen) { return; }
+
+    return (
+      <div className="panel-body">
         <Search onSearch={this.props.onSearch} />
         <Filter onFilter={this.props.onFilter} />
       </div>
     );
+  },
+
+  getInitialState: function () {
+    return { isOpen: false };
+  },
+
+  toggleOpen: function () {
+    this.setState({ isOpen: !this.state.isOpen });
   }
 });
